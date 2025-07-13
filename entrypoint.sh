e80530dce69f674483b0aee1c1f4a22bbc62bad3
Christoph Aldrian <190271467+caldrianx@users.noreply.github.com>
2025-07-13 15:38:33 +0200

    delete envoy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Start Envoy in the background
-envoy --log-level debug -c /etc/envoy/envoy.yaml &
+# envoy --log-level debug -c /etc/envoy/envoy.yaml &
 
 # Start the ASP.NET Core app
-ASPNETCORE_HTTP_PORTS=5000 exec dotnet MugMiles.dll
+#ASPNETCORE_HTTP_PORTS=5000 \
+exec dotnet MugMiles.dll
